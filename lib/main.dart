476128c131bc2a1965616974267f016d82ffa0e4
Heba Mohamed <heba.odes.tasks@gmail.com>
2024-05-13 00:05:18 +0300

    create function

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,20 @@ void main() {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [            
-              Container(
+           getContainer('https://cdn.romanspizza.co.za/images/root/v2/pizza/pizza-vegetarian-pan.png', 'Vegetable Pizza'),
+           getContainer('https://cdn.romanspizza.co.za/images/root/v2/pizza/pizza-classic-pan.png', 'Cheese Pizza'),
+           getContainer('https://cdn.icon-icons.com/icons2/236/PNG/256/FastFood_FrenchFries_26372.png', 'Boxd of Fries'),
+     ],
+          ),
+        ),
+          ),
+        ),
+        ),
+        );
+}
+
+Widget getContainer(String imgPath, String title ){
+return  Container(
                 margin: const EdgeInsets.all(15),
                 padding: const EdgeInsets.all(20),
                 height: 150,
@@ -32,91 +45,17 @@ void main() {
               SizedBox(
                 width: 100,
                 height: 100,
-                child: Image.network('https://cdn.romanspizza.co.za/images/root/v2/pizza/pizza-vegetarian-pan.png',
+                child: Image.network(imgPath,
                 fit: BoxFit.contain,),
               ),
-              const SizedBox(width: 50,),
-                const Text('Vegetable Pizza', 
+              const SizedBox(width: 30,),
+                Text(title, 
                 style: TextStyle(color: Colors.white, 
-                fontWeight: FontWeight.bold, 
+                fontWeight: FontWeight.bold,
+                fontSize: 20, 
                 ),
                 ),
              ],
             ),
-          ),
-          Container(
-                margin: const EdgeInsets.all(15),
-                padding: const EdgeInsets.all(20),
-                height: 150,
-                width: 400,
-                decoration: BoxDecoration(
-                color: Colors.orange,
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: const [
-                  BoxShadow(
-                    offset: Offset(0, 2),
-                    color: Colors.black12,
-                    spreadRadius: 5,
-                    blurRadius: 5,
-                  ),
-                ],            
-              ),
-             child:Row(
-              mainAxisAlignment: MainAxisAlignment.start, 
-             children: <Widget>[
-              Flexible(
-                child: Image.network('https://cdn.romanspizza.co.za/images/root/v2/pizza/pizza-classic-pan.png',
-                ),
-              ),
-              const SizedBox(width: 50,),
-                const Text('Cheese Pizza', 
-                style: TextStyle(color: Colors.white, 
-                fontWeight: FontWeight.bold, 
-                ),
-                ),
-             ],
-            ),
-          ),
-          Container(
-                margin: const EdgeInsets.all(15),
-                padding: const EdgeInsets.all(20),
-                height: 150,
-                width: 400,                
-                decoration: BoxDecoration(
-                color: Colors.orange,
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: const [
-                  BoxShadow(
-                    offset: Offset(0, 2),
-                    color: Colors.black12,
-                    spreadRadius: 5,
-                    blurRadius: 5,
-                  ),
-                ],            
-              ),
-             child:Row(
-              mainAxisAlignment: MainAxisAlignment.start, 
-             children: <Widget>[
-              Flexible(
-                child: Image.network('https://cdn.icon-icons.com/icons2/236/PNG/256/FastFood_FrenchFries_26372.png',
-                ),
-              ),
-              const SizedBox(width: 50,),
-                const Text('Box of Fries', 
-                style: TextStyle(color: Colors.white, 
-                fontWeight: FontWeight.bold, 
-                ),
-                ),
-             ],
-            ),
-          ),
-          
-            ],
-          ),
-        ),
-          ),
-        ),
-        ),
-        );
+          ) ;
 }
-
