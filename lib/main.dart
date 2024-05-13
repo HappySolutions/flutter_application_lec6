de90ca7ddbbfc98c5962ab43dd88fb9a7287a0ae
Heba Mohamed <heba.odes.tasks@gmail.com>
2024-05-13 20:35:49 +0300

    code refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_lec6/my_card.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +22,7 @@ class MyApp extends StatelessWidget {
       actions: [
         IconButton(onPressed: (){}, icon: const Icon(Icons.facebook_outlined, 
         color: Colors.white,)),
-        IconButton(onPressed: (){}, icon: const Icon(Icons.access_alarm_rounded, 
+        IconButton(onPressed: (){}, icon: const Icon(Icons.facebook_sharp, 
         color: Colors.white,)),
       ],
       ),
@@ -32,17 +31,27 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-               Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+               ButtonBar(
+                alignment: MainAxisAlignment.center,
                 children: [
-                  OutlinedButton(onPressed: (){}, child: const Text('data')),
-                  OutlinedButton(onPressed: (){}, child: const Text('data')),
-                  OutlinedButton(onPressed: (){}, child: const Text('data')),
+                  OutlinedButton(
+                    style: const ButtonStyle(side: MaterialStatePropertyAll(BorderSide(color: Colors.orange))),
+                    onPressed: (){}, child: const Text('Vegetable Pizza', style: TextStyle(color: Colors.orange, fontWeight: FontWeight.bold),)),
+                  OutlinedButton(
+                     style: const ButtonStyle(side: MaterialStatePropertyAll(BorderSide(color: Colors.orange))),
+                    onPressed: (){}, child: const Text('Cheese Pizza', style: TextStyle(color: Colors.orange, fontWeight: FontWeight.bold),)),
+                  OutlinedButton(
+                     style: const ButtonStyle(side: MaterialStatePropertyAll(BorderSide(color: Colors.orange))),
+                    onPressed: (){}, child: const Text('Fries', style: TextStyle(color: Colors.orange, fontWeight: FontWeight.bold),)),
                 ],
               ),
-              Image.network('https://cdn.romanspizza.co.za/images/root/v2/pizza/pizza-vegetarian-pan.png',
-              fit: BoxFit.fill,),
-              const Text('Hi, I\'m the Pizza Assistant, What can I help you order?',
+              Image.network('https://www.allrecipes.com/thmb/0xH8n2D4cC97t7mcC7eT2SDZ0aE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/6776_Pizza-Dough_ddmfs_2x1_1725-fdaa76496da045b3bdaadcec6d4c5398.jpg',
+              fit: BoxFit.cover,
+              height: 500,
+              width: 500,
+              ),
+    
+              const Text('Hi, I\'m the Pizza Assistant,\n What can I help you order?',
               style: TextStyle(
                 color: Colors.black,
                 fontSize: 22,
@@ -50,6 +59,8 @@ class MyApp extends StatelessWidget {
 
 
            /* MyCard(imgPath: 'https://cdn.romanspizza.co.za/images/root/v2/pizza/pizza-vegetarian-pan.png',title: 'Vegetable Pizza' ),
+           
+
            MyCard(imgPath: 'https://cdn.romanspizza.co.za/images/root/v2/pizza/pizza-classic-pan.png',title: 'Cheese Pizza'),
            MyCard(imgPath: 'https://cdn.icon-icons.com/icons2/236/PNG/256/FastFood_FrenchFries_26372.png',title: 'Boxd of Fries'), */
              ],
