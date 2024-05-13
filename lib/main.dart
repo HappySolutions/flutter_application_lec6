331f40b6d8e407fbfee77d034a007c5cf321a863
Heba Mohamed <heba.odes.tasks@gmail.com>
2024-05-13 20:48:53 +0300

    code refactor

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
+              SizedBox(height: 10,),
                ButtonBar(
                 alignment: MainAxisAlignment.center,
                 children: [
@@ -45,6 +46,7 @@ class MyApp extends StatelessWidget {
                     onPressed: (){}, child: const Text('Fries', style: TextStyle(color: Colors.orange, fontWeight: FontWeight.bold),)),
                 ],
               ),
+              SizedBox(height: 10,),
               Image.network('https://www.allrecipes.com/thmb/0xH8n2D4cC97t7mcC7eT2SDZ0aE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/6776_Pizza-Dough_ddmfs_2x1_1725-fdaa76496da045b3bdaadcec6d4c5398.jpg',
               fit: BoxFit.cover,
               height: 500,
