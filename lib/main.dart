700dec9eb717b81c63e4e1a3ee6db6fcbebdb3de
Heba Mohamed <heba.odes.tasks@gmail.com>
2024-05-13 16:30:25 +0300

    task7

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,46 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
-      body: SingleChildScrollView(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: const Text('WOW Pizza', style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        color: Colors.white,
+      ),
+      ),
+      actions: [
+        IconButton(onPressed: (){}, icon: const Icon(Icons.facebook_outlined, 
+        color: Colors.white,)),
+        IconButton(onPressed: (){}, icon: const Icon(Icons.access_alarm_rounded, 
+        color: Colors.white,)),
+      ],
+      ),
+      body:  SingleChildScrollView(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [            
-           MyCard(imgPath: 'https://cdn.romanspizza.co.za/images/root/v2/pizza/pizza-vegetarian-pan.png',title: 'Vegetable Pizza' ),
+            children: [
+               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  OutlinedButton(onPressed: (){}, child: const Text('data')),
+                  OutlinedButton(onPressed: (){}, child: const Text('data')),
+                  OutlinedButton(onPressed: (){}, child: const Text('data')),
+                ],
+              ),
+              Image.network('https://cdn.romanspizza.co.za/images/root/v2/pizza/pizza-vegetarian-pan.png',
+              fit: BoxFit.fill,),
+              const Text('Hi, I\'m the Pizza Assistant, What can I help you order?',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+              ),),    
+
+
+           /* MyCard(imgPath: 'https://cdn.romanspizza.co.za/images/root/v2/pizza/pizza-vegetarian-pan.png',title: 'Vegetable Pizza' ),
            MyCard(imgPath: 'https://cdn.romanspizza.co.za/images/root/v2/pizza/pizza-classic-pan.png',title: 'Cheese Pizza'),
-           MyCard(imgPath: 'https://cdn.icon-icons.com/icons2/236/PNG/256/FastFood_FrenchFries_26372.png',title: 'Boxd of Fries'),
+           MyCard(imgPath: 'https://cdn.icon-icons.com/icons2/236/PNG/256/FastFood_FrenchFries_26372.png',title: 'Boxd of Fries'), */
              ],
           ),
         ),
